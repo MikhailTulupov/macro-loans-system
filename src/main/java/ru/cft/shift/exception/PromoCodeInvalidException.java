7368f03223efec3ecfc55ea9d440fe800cbfa319
Alexey Horoshavin <Akela.Klimov@yandex.ru>
2022-07-15 08:46:13 +0700

    remove unused code

--- src/main/java/ru/cft/shift/exception/PromoCodeInvalidException.java
+++
@@ -1,8 +0,0 @@
-package ru.cft.shift.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "promo code is invalid")
-public class PromoCodeInvalidException extends Exception{
-}
